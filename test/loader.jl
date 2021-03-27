956d407959725bcb3e141e5401e7177eb04226c2
Abhi Sinha <sinha45@purdue.edu>
2021-03-27 01:09:21 -0400

    update tests julia

--- test/loader.jl
+++ test/loader.jl
@@ -1,19 +1,31 @@
+one_away_data = UInt8.(cat([0 1; 1 1], [1 2; 0 0], dims=3))
+one_away_ret = [56 13.856406 11.313708]
+
+two_away_data = UInt8.(cat([0 1 2 1 0; 1 1 1 1 1; 0 0 0 0 0], 
+                           [1 2 3 4 5; 0 0 252 0 0; 1 1 1 1 1], dims=3))
+two_away_ret = [654 109.352641 479834.242936]
+
 @testset "singlethread loader" begin
-  data = UInt8.(cat([0 1; 1 1], [1 2; 0 0], dims=3))
-  image = Image{UInt8}(data, 1)
+  image = Image{UInt8}(one_away_data, 1)
   im2gr!(image, IM2GR.SingleThread)
-  @test length(image.ei) == 56 && length(image.ej) == 56
+  @test begin 
+    length(image.ei) == one_away_ret[1] && 
+    length(image.ej) == one_away_ret[1] 
+  end
   @test begin
-    isapprox(norm(image.evd), 13.856406, atol=1e-6) && 
-    isapprox(norm(image.evi), 11.313708, atol=1e-6)
+    isapprox(norm(image.evd), one_away_ret[2], atol=1e-6) && 
+    isapprox(norm(image.evi), one_away_ret[3], atol=1e-6)
   end
 
-  data = UInt8.(cat([0 1 2 1 0; 1 1 1 1 1; 0 0 0 0 0], [1 2 3 4 5; 0 0 252 0 0; 1 1 1 1 1], dims=3))
-  image = Image{UInt8}(data, 2)
+  image = Image{UInt8}(two_away_data, 2)
   im2gr!(image, IM2GR.SingleThread)
-  @test length(image.ei) == 654 && length(image.ej) == 654
+  @test begin 
+    length(image.ei) == two_away_ret[1] && 
+    length(image.ej) == two_away_ret[1] 
+  end
   @test begin
-    isapprox(norm(image.evd), 109.352641, atol=1e-6) && 
-    isapprox(norm(image.evi), 479834.242936, atol=1e-6)
+    isapprox(norm(image.evd), two_away_ret[2], atol=1e-6) && 
+    isapprox(norm(image.evi), two_away_ret[3], atol=1e-6)
   end
 end
+
