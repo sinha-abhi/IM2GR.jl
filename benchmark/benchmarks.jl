1a69f2f1df63c5ef4fe6b2664f8dcac4838a479a
Abhi Sinha <sinha45@purdue.edu>
2021-04-22 21:49:59 -0400

    bm

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -13,7 +13,6 @@ BenchmarkTools.DEFAULT_PARAMETERS.seconds = 300
 
 println("overhead = ", BenchmarkTools.DEFAULT_PARAMETERS.overhead)
 
-#=
 mri_diff_fn(xi, xj) = min(sqrt(xi) / 63, 1.0) - min(sqrt(xj) / 63, 1.0)
 mri = load("data/lgemri.nrrd")
 
@@ -23,7 +22,7 @@ println("Benchmarking SingleThread")
 stb = @benchmarkable im2gr($mri, 2, IM2GR.CM_SingleThread, $mri_diff_fn)
 stb_ret = run(stb)
 println("Saving results")
-stb_res = open("benchmark/singlethread-mri.txt", "w+")
+stb_res = open("benchmark/singlethread-mri-arrowhead.txt", "w+")
 show(stb_res, MIME"text/plain"(), stb_ret)
 
 println("Running once to compile")
@@ -32,9 +31,8 @@ println("Benchmarking MultiThread")
 mtb= @benchmarkable im2gr($mri, 2, IM2GR.CM_MultiThread, $mri_diff_fn)
 mtb_ret = run(mtb)
 println("Saving results")
-mtb_res = open("benchmark/multithread-mri.txt", "w+")
+mtb_res = open("benchmark/multithread-mri-arrowhead.txt", "w+")
 show(mtb_res, MIME"text/plain"(), mtb_ret)
-=#
 
 #=
 println("Benchmarking CUDA")
@@ -48,14 +46,13 @@ show(cb_res, MIME"text/plain"(), cb_ret)
 sz = (144, 144, 22)
 fake = rand(UInt8, sz)
 
-#=
 println("Running once to compile")
 im2gr(fake, 1, IM2GR.CM_SingleThread)
 println("Benchmarking... SingleThread")
 fstb = @benchmarkable im2gr($fake, 1, IM2GR.CM_SingleThread)
 fstb_ret = run(fstb)
 println("Saving results")
-fstb_res = open("benchmark/singlethread-fake.txt", "w+")
+fstb_res = open("benchmark/singlethread-fake-arrowhead.txt", "w+")
 show(fstb_res, MIME"text/plain"(), fstb_ret)
 
 println("Running once to compile")
@@ -64,10 +61,10 @@ println("Benchmarking... MultiThread")
 fmtb = @benchmarkable im2gr($fake, 1, IM2GR.CM_MultiThread)
 fmtb_ret = run(fmtb)
 println("Saving results")
-fmtb_res = open("benchmark/multithread-fake.txt", "w+")
+fmtb_res = open("benchmark/multithread-fake-arrowhead.txt", "w+")
 show(fmtb_res, MIME"text/plain"(), fmtb_ret)
-=#
 
+#=
 println("Running once to compile")
 im2gr(fake, 1, IM2GR.CM_CUDA)
 println("Benchmarking... CUDA")
@@ -76,3 +73,4 @@ fcb_ret = run(fcb)
 println("Saving results")
 fcb_res = open("benchmark/cuda-fake-17-blocks.txt", "w+")
 show(fcb_res, MIME"text/plain"(), fcb_ret)
+=#
